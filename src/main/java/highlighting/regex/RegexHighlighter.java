cafe3a198ecfb43cbfd35560b1e20312076fcc1f
sevenofeleven <marlenebusch@georgemail.de>
2026-05-28 13:07:05 +0200

    aufgabe 2

--- src/main/java/highlighting/regex/RegexHighlighter.java
+++ src/main/java/highlighting/regex/RegexHighlighter.java
@@ -2,7 +2,10 @@ package highlighting.regex;
 
 import highlighting.core.HighlightRegion;
 import highlighting.core.SyntaxHighlighter;
+import highlighting.presets.MiniJavaTokens;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 // TODO: Implement a simple regex-based highlighting strategy. Unlike the scanning approach, this
 // strategy applies each token independently to the entire input text and collects all resulting
@@ -11,13 +14,27 @@ import java.util.List;
 // TODO: Make this class extend {@code SyntaxHighlighter}, implement the abstract method {@code
 // collectMatches}, and override {@code resolveConflicts} to handle overlapping regions produced by
 // the naive regex-based strategy.
+
+// alle Token aus MiniJavaTokens auf den Eingabetext angewendet werden,
+// alle gefundenen Regionen in einer gemeinsamen Liste gesammelt werden,
+// die Liste zurückgegeben wird (Sortierung übernimmt normalize).
 public class RegexHighlighter extends SyntaxHighlighter {
 
   // TODO: For each token, find all matches of its pattern in the input text, convert them into
   // {@code HighlightRegion}s, and combine all of these regions into a single list.
   @Override
   public List<HighlightRegion> collectMatches(String text) {
-    throw new UnsupportedOperationException("not implemented yet");
+    List<HighlightRegion> regions = new ArrayList<>();
+
+    List<Token> tokens = MiniJavaTokens.defaultTokens();
+    for (Token t : tokens) {
+      Matcher m = t.pattern().matcher(text);
+      while (m.find()) {
+        regions.add(new HighlightRegion(m.start(), m.end(), t.colour()));
+      }
+    }
+
+    return regions;
   }
 
   // TODO: Resolve overlapping regions. Assume that {@code regions} has been normalised and sorted.
@@ -26,6 +43,19 @@ public class RegexHighlighter extends SyntaxHighlighter {
   // position are preferred because of the sorting in {@code normalize}.
   @Override
   public List<HighlightRegion> resolveConflicts(List<HighlightRegion> regions) {
-    throw new UnsupportedOperationException("not implemented yet");
+    List<HighlightRegion> newRegions = new ArrayList<>();
+
+    int i = 0;
+    while (i < regions.size()) {
+      HighlightRegion r = regions.get(i);
+      newRegions.add(r);
+
+      i++;
+      // skip ..
+      while (i < regions.size() && regions.get(i).start() < r.end()) {
+        i++;
+      }
+    }
+    return newRegions;
   }
 }
